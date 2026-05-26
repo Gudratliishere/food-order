f4bf7aaa6f19e16f98875eaf0da1818389a04ee6
Dunay Gudratli <dqudretli@std.beu.edu.az>
2026-05-26 17:04:37 +0400

    298: handle HTTP Loading & Error states

--- src/components/Meals.jsx
+++ src/components/Meals.jsx
@@ -1,5 +1,6 @@
 import MealItem from "./MealItem.jsx";
 import useHttp from "../hooks/useHttp.js";
+import Error from "./Error.jsx";
 
 const requestConfig = {};
 
@@ -8,7 +9,11 @@ export default function Meals() {
         useHttp('http://localhost:3000/meals', requestConfig, []);
 
     if (isLoading) {
-        return <p>Loading...</p>
+        return <p className="center">Loading...</p>
+    }
+
+    if (error) {
+        return <Error title="Failed to fetch meals" message={error}/>
     }
 
     return <ul id="meals">
